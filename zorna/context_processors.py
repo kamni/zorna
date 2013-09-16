a2216fa6a760ed99519cb9a0fe48b7f7d7293f1d
Noureddine Ayachi <nayachi@afraz.fr>
2013-09-16 09:28:40 +0200

    Added user calendar to request context

--- zorna/context_processors.py
+++ zorna/context_processors.py
@@ -4,6 +4,7 @@ from zorna.acl.models import get_allowed_objects
 from django.contrib.sites.models import Site
 
 from zorna.site.models import SiteRegistration
+from zorna.calendars.api import get_personal_calendar
 
 
 def alerts(request):
@@ -32,9 +33,11 @@ def zsettings(request):
 
     if request.user.is_anonymous():
         usergroups = []
+        calendar = None
     else:
         usergroups = [str(g.pk)
                       for g in request.user.get_profile().groups.all()]
+        calendar = get_personal_calendar(request.user)
 
     return {'CKEDITOR_MEDIA_PREFIX': settings.CKEDITOR_MEDIA_PREFIX,
             'ZORNA_SKIN': settings.ZORNA_SKIN,
